e06c3c0ba7ced077a6800c56d250bca42b90ad39
tleish <tleish@users.noreply.github.com>
2020-02-12 10:11:25 -0700

    refactor mixin to allow global and single component mixin patterns as discussed in #31

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-function handleVueDestructionOn(turbolinksEvent, vue) {
+function handleVueDestruction(vue) {
+  var turbolinksEvent = vue.$options.turbolinksDestroyEvent || 'turbolinks:visit';
   document.addEventListener(turbolinksEvent, function teardown() {
     vue.$destroy();
     document.removeEventListener(turbolinksEvent, teardown);
   });
 }
 
-function plugin(Vue, options) {
-  // Install a global mixin
-  Vue.mixin({
-
-    beforeMount: function() {
-      // If this is the root component, we want to cache the original element contents to replace later
-      // We don't care about sub-components, just the root
-      if (this == this.$root && this.$el) {
-        var destroyEvent = this.$options.turbolinksDestroyEvent || 'turbolinks:visit'
-        handleVueDestructionOn(destroyEvent, this);
-        this.$turbolinksCachedHTML = this.$el.outerHTML;
-        // register root hook to restore original element on destroy
-        this.$once('hook:destroyed', function() {
-          this.$el.outerHTML = this.$turbolinksCachedHTML
-        });
-      }
-    },
-  })
+var turbolinksAdapterMixin = {
+  beforeMount: function() {
+    // If this is the root component, we want to cache the original element contents to replace later
+    // We don't care about sub-components, just the root
+    if (this === this.$root && this.$el) {
+      handleVueDestruction(this);
+      // cache original element
+      this.$turbolinksCachedHTML = this.$el.outerHTML;
+      // register root hook to restore original element on destroy
+      this.$once('hook:destroyed', function() {
+        this.$el.outerHTML = this.$turbolinksCachedHTML
+      });
+    }
+  }
 };
 
+function plugin(Vue, options) {
+  // Install a global mixin
+  Vue.mixin(turbolinksAdapterMixin)
+}
+
+export { turbolinksAdapterMixin };
 export default plugin;
